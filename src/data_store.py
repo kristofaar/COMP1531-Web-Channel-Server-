12dcfe4d3ce6b2279a8da18e803fee808306d701
Alex <z5316722@ad.unsw.edu.au>
2022-02-26 21:49:42 +1100

    Removed assumption that users cannot be deleted

--- src/data_store.py
+++ src/data_store.py
@@ -26,7 +26,9 @@ Example usage:
 
 ## YOU SHOULD MODIFY THIS OBJECT BELOW
 initial_object = {
+    #users will contain a list of dictionaries containing in order: user id, email, first name, last name, handle
     'users': [],
+    #passwords contain a list of dictionaries containing in order: user id, password
     'passwords': [],
 }
 ## YOU SHOULD MODIFY THIS OBJECT ABOVE
