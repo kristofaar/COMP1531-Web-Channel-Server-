df74f7e081bb0ea0cb2dd0ac6e4908b7845c3864
Alexander Gerlyand <z5388158@ad.unsw.edu.au>
2022-02-28 19:44:32 +1100

    started channel and channel_test

--- src/channel.py
+++ src/channel.py
@@ -1,8 +1,50 @@
+from src.data_store import data_store
+from src.error import InputError
+from src.error import AccessError
+
+'''
+channel_invite_v1 invites a user with ID u_id to join a channel with ID channel_id. Once invited, the user is 
+added to the channel immediately. In both public and private channels, all members are able to invite users.
+
+Arguments:
+    auth_user_id (int)    - passes in the unique user id of whoever ran the funtion
+    channel_id   (int)    - passes in the unique channel id of the channel we are inviting someone to
+    u_id         (int)    - passes in the unique user id of who we are inviting into the channel
+
+Exceptions:
+    InputError  - Occurs when channel_id does not refer to a valid channel
+                - Occurs when u_id does not refer to a valid user
+                - Occurs when u_id refers to a user who is already a member of the channel
+    AccessError - Occurs when channel_id is valid and the authorised user is not a member of the channel
+
+Return Value:
+    Returns <return value> on <condition>
+    Returns <return value> on <condition>
+'''
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
 
+'''
+channel_details_v1 provides basic details about the channel given a channel with ID channel_id that the 
+authorised user is a member of.
+
+Arguments:
+    auth_user_id (int)    - passes in the unique user id of whoever ran the funtion
+    channel_id   (int)    - passes in the unique channel id of the channel we are enquiring about
+
+Exceptions:
+    InputError  - Occurs when channel_id does not refer to a valid channel
+    AccessError - Occurs when channel_id is valid and the authorised user is not a member of the channel
+
+Return Value:
+    Returns <return value> on <condition>
+    Returns <return value> on <condition>
+'''
 def channel_details_v1(auth_user_id, channel_id):
+    #staging variables
+    storage = data_store.get()
+    return storage['channels'][channel_id]
     return {
         'name': 'Hayden',
         'owner_members': [
@@ -25,6 +67,22 @@ def channel_details_v1(auth_user_id, channel_id):
         ],
     }
 
+'''
+<Brief description of what the function does>
+
+Arguments:
+    <name> (<data type>)    - <description>
+    <name> (<data type>)    - <description>
+    ...
+
+Exceptions:
+    InputError  - Occurs when ...
+    AccessError - Occurs when ...
+
+Return Value:
+    Returns <return value> on <condition>
+    Returns <return value> on <condition>
+'''
 def channel_messages_v1(auth_user_id, channel_id, start):
     return {
         'messages': [
@@ -39,6 +97,21 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         'end': 50,
     }
 
+'''<Brief description of what the function does>
+
+Arguments:
+    <name> (<data type>)    - <description>
+    <name> (<data type>)    - <description>
+    ...
+
+Exceptions:
+    InputError  - Occurs when ...
+    AccessError - Occurs when ...
+
+Return Value:
+    Returns <return value> on <condition>
+    Returns <return value> on <condition>
+'''
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
---
+++ tests/channel_test.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.channels import channels_create_v1,channels_listall_v1,channels_list_v1
+from src.channel import channel_details_v1, channel_invite_v1, channel_join_v1, channel_messages_v1
+from src.auth import auth_register_v1, auth_login_v1
+from src.error import InputError
+from src.error import AccessError
+from src.other import clear_v1
+
+#Error tests
+'''
+def test_invalid_user_id():
+    clear_v1()
+    with pytest.raises(InputError):
+        channel_invite_v1(0, 1, 1)
+
+def test_invalid_channel_id():
+    clear_v1()
+    with pytest.raises(AccessError):
+        channel_invite_v1(1,0,1)
+
+def test_invalid_user_id():
+    clear_v1()
+    with pytest.raises(InputError):
+        channel_invite_v1(1,1,0)
+'''
+#Working tests
