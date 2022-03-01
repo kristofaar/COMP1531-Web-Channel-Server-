1eb7c97f410028ca9db9a5a94c46811a9bae1dee
Alex <z5316722@ad.unsw.edu.au>
2022-03-01 12:26:55 +1100

    in the middle of adding a test

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -6,6 +6,7 @@ from src.auth import auth_register_v1, auth_login_v1
 from src.error import InputError
 from src.error import AccessError
 from src.other import clear_v1
+from src.data_store import data_store
 
 #Error tests
 '''
@@ -36,5 +37,12 @@ def test_channel_messages_invalid_channel_id_empty():
     auth_register_v1('anemail@email.com', 'verycoolpassword', 'Name', 'Name')
     with pytest.raises(InputError):
         channel_messages_v1(1, 3, 0)
-        
+
+def test_channel_messages_invalid_start():
+    clear_v1()
+    auth_register_v1('anemail@email.com', 'verycoolpassword', 'Name', 'Name')
+    channels_create_v1(1, 'channelName', True)
+    storage = data_store.get()
+    
+
 #Working tests
