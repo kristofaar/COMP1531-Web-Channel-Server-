5f56f04a7739c3df13b65d0b1f010f989bf993e0
Kristofa <z5310212@ad.unsw.edu.au>
2022-03-01 11:37:46 +1100

    moved my test case from file channels_test to channel_test

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -6,7 +6,7 @@ from src.auth import auth_register_v1, auth_login_v1
 from src.error import InputError
 from src.error import AccessError
 from src.other import clear_v1
-
+from src.data_store import data_store
 #Error tests
 '''
 def test_invalid_user_id():
@@ -25,3 +25,18 @@ def test_invalid_user_id():
         channel_invite_v1(1,1,0)
 '''
 #Working tests
+
+def test_channel_list_details_public_private():
+    clear_v1()
+    auth_register_v1('anemail@email.com', 'verycoolpassword', 'Name', 'Name')
+    channels_create_v1(1, 'public', True)
+    channels_create_v1(1, 'private', False)
+
+    storage = data_store.get()
+    users = storage['users']
+    channels = storage['channels']
+    for channel in channels:
+        if channel['name'] == 'public':
+            assert channel['is_public'] == True
+        elif channel['name'] == 'private':
+            assert channel['is_public'] == False
