ed38c26925be53244d961e7bc129a21a9065b952
Alex <z5316722@ad.unsw.edu.au>
2022-03-04 13:54:14 +1100

    removed data store

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -3,7 +3,6 @@ import pytest
 from src.auth import auth_register_v1, auth_login_v1
 from src.error import InputError
 from src.other import clear_v1
-from src.data_store import data_store
 
 #Error tests
 def test_register_invalid_email():
