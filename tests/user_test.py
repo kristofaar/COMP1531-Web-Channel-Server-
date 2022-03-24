b1fac7b05bc606e4c7c4ef5d3798b7906dafd41f
Alexander Gerlyand <z5388158@ad.unsw.edu.au>
2022-03-24 16:50:28 +1100

    implemented users_all_v1

---
+++ tests/user_test.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+import json
+from src import config
+
+A_ERR = 403
+I_ERR = 400
+OK = 200
+
+@pytest.fixture
+def reg_two_users():
+    clear_resp = requests.delete(config.url + 'clear/v1')
+    assert clear_resp.status_code == OK
+    resp1 = requests.post(config.url + 'auth/register/v2', json={'email': '1@test.test', 'password': '1testtest', 'name_first': 'first_test', 'name_last': 'first_test'})
+    assert resp1.status_code == OK
+    resp2 = requests.post(config.url + 'auth/register/v2', json={'email': '2@lol.lol', 'password': '2abcabc', 'name_first': 'Second_Jane', 'name_last': 'Second_Austen'})
+    assert resp2.status_code == OK
+    resp1_data = resp1.json()
+    resp2_data = resp2.json()
+    return {'token1': resp1_data['token'], 'token2': resp2_data['token'], 
+    'u_id1': resp1_data['auth_user_id'], 'u_id2': resp2_data['auth_user_id']}
+
+def test_users_two_users(reg_two_users):
+    resp1 = requests.get(config.url + 'users/all/v1', params={'token': reg_two_users['token1']})
+    assert resp1.status_code == OK
+    resp1_data = resp1.json()
+    assert resp1_data["users"][0]["u_id"] == reg_two_users["u_id1"]
+    assert resp1_data["users"][1]["u_id"] == reg_two_users["u_id2"]
