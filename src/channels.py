b5937b5d7cd4586c0266bb80bf0f298e9d5c8cf1
Alexander Gerlyand <z5388158@ad.unsw.edu.au>
2022-04-13 22:02:25 +1000

    forgot to add change to channels that was revoked due to merging from master zzz

--- src/channels.py
+++ src/channels.py
@@ -103,7 +103,7 @@ def channels_create_v1(token, name, is_public):
 
     #updating the data store
     channels.append({'channel_id_and_name' :{'channel_id' : ch_id, 'name' : name}, 'is_public' : is_public, 
-    'owner' : [user_id], 'members' : [user_id], 'messages' : []})
+    'owner' : [user_id], 'members' : [user_id], 'messages' : [], "standup_time": 0, "standup_message": ""})
 
     #updating user
     curr_user['channels'].append({'channel_id' : ch_id, 'name' : name})
