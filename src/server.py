46259617fb0a0117b0a85091670d47b352a09c98
Adrian Lim <z5309375@ad.unsw.edu.au>
2022-04-19 01:00:59 +1000

    Fixed uploadphoto/v1 function wrapper

--- src/server.py
+++ src/server.py
@@ -13,7 +13,7 @@ from src.channel import channel_details_v1, channel_invite_v1, channel_join_v1, 
 from src.dm import dm_create_v1, dm_list_v1, dm_remove_v1, dm_details_v1, dm_leave_v1, dm_messages_v1
 from src.other import clear_v1, notifications_get_v1
 from src.message import message_send_v1, message_edit_v1, message_remove_v1, message_senddm_v1,  message_sendlater_v1, message_sendlaterdm_v1, message_share_v1, message_react_v1, message_unreact_v1, message_pin_v1, message_unpin_v1
-from src.user import users_all_v1, user_profile_v1, user_profile_setname_v1, user_profile_setemail_v1, user_profile_sethandle_v1, user_stats_v1, users_stats_v1
+from src.user import users_all_v1, user_profile_v1, user_profile_setname_v1, user_profile_setemail_v1, user_profile_sethandle_v1, user_stats_v1, users_stats_v1, user_profile_uploadphoto_v1
 from src.admin import admin_user_remove_v1, admin_userpermission_change_v1
 from src.standup import standup_start, standup_send, standup_active
 import pickle
